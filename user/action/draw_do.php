d0d1ca9951021a475ae6d47903371f007752e485
eWangsf <1342832773@qq.com>
2014-11-04 22:46:52 +0800

    draw
    draw from system

---
+++ user/action/draw_do.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 小卫卫九块九W
+ * Date: 14-11-2
+ * Time: 下午8:53
+ */
+
+require_once("../../conn/conndb.php");
+session_start();
+$uname = $_SESSION['user'];
+$card = $_POST['aimcard'];
+$num = $_POST['num'];
+$ppwd = $_POST['drawpwd'];
+
+$str = "select * from user where userName='$uname' and payPassword='$ppwd'";
+$arr = mysql_query($str);
+$result = mysql_fetch_array($arr);
+if(mysql_num_rows($arr)>0)
+    /*用户名和支付密码都正确，接下来要找到目标银行卡，更新银行卡余额*/
+    {
+        $str2 = "select userID from user where userName ='$uname'";
+        $arr2 = mysql_query($str2);
+        $result2 = mysql_fetch_array($arr2);
+        $uid = $result2['userID'];
+
+        $str3 = "update account set balance=balance-'$num' where owner='$uid' and accountID='$card'";
+        $arr3 = mysql_query($str3);
+        if($arr3)
+            {
+                echo "<script>alert('success draw!');window.location.href='../view/save.php';</script>";
+            }
+        else
+            {
+                echo "<script>alert('invalid update!please check your card number and confirm that you enter the correct password!');
+                    window.location.href='../view/save.php';</script>";
+            }
+
+    }
+else
+    {
+        echo "<script>alert('invalid update!please check your card number and confirm that you enter the correct password!');
+        window.location.href='../view/save.php';</script>";
+    }
+
+?>
--- user/view/draw.php
+++ user/view/draw.php
@@ -1,10 +1,29 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: 小卫卫九块九W
- * Date: 14-11-2
- * Time: 下午7:15
- */
+<html>
+<head>
+    <meta http-equiv="content-type" content="text/html;charset=utf-8">
+    <link href="http://libs.baidu.com/bootstrap/3.0.3/css/bootstrap.min.css" rel="stylesheet">
+    <script src="http://libs.baidu.com/jquery/2.0.0/jquery.min.js"></script>
+    <script src="http://libs.baidu.com/bootstrap/3.0.3/js/bootstrap.min.js"></script>
+    <link href="../css/draw.css" rel="stylesheet" type="text/css">
+</head>
+<body>
 
-echo "this is the draw page";
-?>
+<div id="wrapper">
+    <div id="head">
+        <p>取款</p>
+    </div>
+    <div id="cont">
+        <form name="drawform" action="../action/draw_do.php" method="post">
+            &nbsp;银&nbsp;行&nbsp;卡&nbsp;号&nbsp;：<input type="text" name="aimcard" id="aimcard" placeholder="银行卡号" ><br/><br/>
+            &nbsp;取&nbsp;出&nbsp;金&nbsp;额&nbsp;：
+            <input type="text" name="num" id="num" placeholder="金额" ><br/><br/>
+            &nbsp;支&nbsp;付&nbsp;密&nbsp;码&nbsp;：
+            <input type="password" name="drawpwd" id="drawpwd" placeholder="密码" ><br/><br/>
+            <input type="submit" value="确认">
+        </form>
+    </div>
+</div>
+
+
+</body>
+</html>
